549184acec71714a9cba260fc332485b4764245b
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2023-01-25 16:19:43 +0200

    change to a function

---
+++ frontend/src/utils/fetchMovieData.js
@@ -0,0 +1,17 @@
+export default function fetchMovieData({ imdbId = "0113497" }) {
+  const movieData = {};
+  const url = "https://search.imdbot.workers.dev/?tt=tt";
+  const getMovieFromImdbId = async (imdbId) => {
+    console.log(url + imdbId);
+    const res = await fetch(url + imdbId);
+    const data = await res.json();
+    console.log("data", data);
+    movieData["imdbId"] = data["imdbId"];
+    movieData["title"] = data.main.titleText.text;
+    movieData["picture"] = data.short.image;
+    movieData["genre"] = data.short.genre;
+    console.log("movieData", movieData);
+  };
+  getMovieFromImdbId(imdbId);
+  return movieData;
+}
