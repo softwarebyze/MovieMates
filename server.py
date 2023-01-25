a5a896c696e9a3c2fe70717da6480d035c8e77c5
Oleg Podlipalin <backspace.by@gmail.com>
2023-01-25 21:43:12 +0200

    tmp folder for files

--- server.py
+++ server.py
@@ -16,10 +16,10 @@ user_item_matrix_file_name = 'user_item_matrix.csv'
 
 s3.download_file(bucket_name, model_file_name, '/tmp/model.pkl')
 model = pickle.load(open('/tmp/model.pkl', 'rb'))
-s3.download_file(bucket_name, matrix_original_file_name, '/matrix_original.csv')
-matrix_original = pd.read_csv('/matrix_original.csv')
-s3.download_file(bucket_name, matrix_original_file_name, '/user_item_matrix.csv')
-user_item_matrix = pd.read_csv('/user_item_matrix.csv')
+s3.download_file(bucket_name, matrix_original_file_name, '/tmp/matrix_original.csv')
+matrix_original = pd.read_csv('/tmp/matrix_original.csv')
+s3.download_file(bucket_name, matrix_original_file_name, '/tmp/user_item_matrix.csv')
+user_item_matrix = pd.read_csv('/tmp/user_item_matrix.csv')
 
 # matrix_original = pd.read_csv('matrix_original.csv')
 # user_item_matrix = pd.read_csv('user_item_matrix.csv')
