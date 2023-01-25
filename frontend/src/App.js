e2dfb35430ff4d5b90ef49594c377cd9ed7b386b
Daveman20 <drdaveman1337@gmail.com>
2023-01-25 13:40:49 +0200

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,20 @@ import "./App.sass";
 import {BrowserRouter, Link, Routes, Route} from 'react-router-dom'
 import Home from "./Components/Home";
 import Profile from "./Components/Profile";
+import GamePage from "./Pages/Game/GamePage";
+import PrivateRoute from "./Components/PrivateRoute";
 
 function App() {
   return (<BrowserRouter>
     <div className="App">
+      <div className="NavBar">
       <Link to='/'>Home</Link>
       <Link to='/profile'>Profile</Link>
+      <Link to='game'>Rate Movies</Link>
+      </div>
       <Routes><Route path="/" element={<Home/>} />
-      <Route path="/profile" element={<Profile/>}/></Routes>
+      <Route path="/profile" element={<PrivateRoute><Profile/></PrivateRoute>}/>
+      <Route path="/game" element={<PrivateRoute><GamePage /></PrivateRoute>} /></Routes>
       {/* <div className="NavBar">
         <Link to="/">Home</Link>
         <Link to="/game">Rate Movies</Link>
--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import AppContext from "../AppContext";
+// import AppContext from "../AppContext";
 
 const PrivateRoute = ({ children }) => {
-  const { currentUser } = useContext(AppContext); // when user is logged in have the state be changed
-  return currentUser ? children : <Navigate to="/login"></Navigate>;
+  const currentUser  = true //useContext(AppContext); // when user is logged in have the state be changed
+  return currentUser ? children : <Navigate to="/"></Navigate>;
 };
 
 export default PrivateRoute;
