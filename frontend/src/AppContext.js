e3ed6da11b646b1ec27535693c58862beceede0c
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2023-01-25 17:01:22 +0200

    add rateMovie func

--- frontend/src/AppContext.js
+++ frontend/src/AppContext.js
@@ -1,24 +1,34 @@
+import { doc, setDoc } from "firebase/firestore";
 import { createContext, useState } from "react";
+import { db } from "../firebase";
 
 const AppContext = createContext();
 
+export function AppProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState("false");
+  const handleLogout = (e) => {
+    //signout from db
+    console.log("test");
+    setCurrentUser(false);
+  };
 
-export function AppProvider({children}){
-    const [currentUser, setCurrentUser] = useState('false')
-    const handleLogout = (e)=>{
-        //signout from db
-        console.log('test')
-         setCurrentUser(false)
+  const rateMovie = async (userId, movieId, rating) => {
+    const ratingObject = {};
+    ratingObject[movieId] = rating;
+    // send {movieId: movieId, rating: rating} to data scientists
+    try {
+      // update firestore with new rating
+      await setDoc(doc(db, "users", userId), ratingObject);
+    } catch (e) {
+      console.error("Error adding document: ", e);
     }
-    
-    const rateMovie = (userId, movieId, rating) => {
-        // send {movieId: movieId, rating: rating} to data scientists
-        // update firestore
-    }
-    
-    return(<AppContext.Provider value={{currentUser, handleLogout}}>{children}</AppContext.Provider>);}
+  };
 
-
-
+  return (
+    <AppContext.Provider value={{ currentUser, handleLogout, rateMovie }}>
+      {children}
+    </AppContext.Provider>
+  );
+}
 
 export default AppContext;
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -19,4 +19,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-const db = getFirestore(app);
+export const db = getFirestore(app);
