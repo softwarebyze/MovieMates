1f57ae35d24f7f48e9cc25846b45f88065652f5b
kgoodluck <k-goodluck@yandex.ru>
2023-01-25 11:32:10 +0200

    added basic components for the game page

---
+++ frontend/src/Components/Game/Grades.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Grades() {
+  return (
+    <div>
+        <button>☹</button>
+        <button>🙁</button>
+        <button>😐</button>
+        <button>🙂</button>
+        <button>😊</button>
+    </div>
+  )
+}
--- frontend/src/Pages/Game/GamePage.jsx
+++ frontend/src/Pages/Game/GamePage.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Carousel from '../../Components/Game/Carousel'
+import Grades from '../../Components/Game/Grades'
+import ProgressBar from '../../Components/Game/ProgressBar'
 
 export default function GamePage() {
+
+    
+
+
   return (
-    <div>GamePage</div>
+    <div className='d-flex flex-column'>
+        <ProgressBar />
+        <Carousel />
+        <Grades />
+    </div>
   )
 }
