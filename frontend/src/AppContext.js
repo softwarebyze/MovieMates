4bfdd4fb3051671d3bda55baaef9cde9c0f2d62a
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2023-01-25 16:15:11 +0200

    rateMovie function

--- frontend/src/AppContext.js
+++ frontend/src/AppContext.js
@@ -9,7 +9,12 @@ export function AppProvider({children}){
         //signout from db
         console.log('test')
          setCurrentUser(false)
-      }
+    }
+    
+    const rateMovie = (userId, movieId, rating) => {
+        // send {movieId: movieId, rating: rating} to data scientists
+        // update firestore
+    }
     
     return(<AppContext.Provider value={{currentUser, handleLogout}}>{children}</AppContext.Provider>);}
 
