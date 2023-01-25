7eeafe9cf37a0c8ff58ea6b28e551561f87db511
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2023-01-25 09:42:42 +0200

    add firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARgfba4J4IoSMyZx1ZrpXUD0LpLs8ovhE",
+  authDomain: "moviemates-c4e06.firebaseapp.com",
+  projectId: "moviemates-c4e06",
+  storageBucket: "moviemates-c4e06.appspot.com",
+  messagingSenderId: "214915447321",
+  appId: "1:214915447321:web:58f9a0e173d1763ca98888"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
