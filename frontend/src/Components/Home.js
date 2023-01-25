1db1a35a38eb49a86bb2b53b7018b4dcb2e86401
Daveman20 <drdaveman1337@gmail.com>
2023-01-25 20:08:48 +0200

    made a small Logged in homepage

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import Auth from "./auth/Auth";
 import { useState } from "react";
+import Logo from "../Elements/Logo";
+import '../App.css'
 
 const Home = () => {
   const [isAuth, setIsAuth] = useState(false);
@@ -14,7 +16,7 @@ const Home = () => {
     }
   }, []);
 
-  return <div>{isAuth ? <div>You are login</div> : <Auth />}</div>;
+  return <div>{!isAuth ?  <Auth />: <div className="loggedHome"><Logo type='vertical' /><div className="appDesc"><p>Welcome to MovieMates!</p> Don't you hate it when you waste hours looking for the perfect movie? Well worry not, with a few simple clicks MovieMates will figure out the movies that best suit you. So what are you waiting for? Sit back, relax, grab your mate and let's watch a movie! </div></div> }</div>;
 };
 
 export default Home;
