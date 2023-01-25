d1bc70c5df8e5a7f779864e6ab7e4db3efbace90
Oleg Podlipalin <backspace.by@gmail.com>
2023-01-25 11:28:46 +0200

    server.py version for aws

---
+++ server.py
@@ -0,0 +1,27 @@
+import pickle
+import pandas as pd
+from flask import Flask, request, jsonify
+import json
+import boto3
+
+app = Flask(__name__)
+
+# s3 = boto3.client('s3')
+# bucket_name = 'flask-itc'
+# file_name = 'churn_model.pkl'
+# s3.download_file(bucket_name, file_name, '/tmp/model.pkl')
+# clf = pickle.load(open('/tmp/model.pkl', 'rb'))
+
+
+@app.route('/mates', methods=['POST'])
+def find_movie_mates():
+    json_data = request.get_json()
+    print(json_data)
+
+    processed_data = json_data['input'] * 2
+
+    return jsonify(output=processed_data)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
