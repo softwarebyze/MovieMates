527d27a50f30d805466393730d1906969d006d2c
kgoodluck <k-goodluck@yandex.ru>
2023-01-25 11:50:35 +0200

    add functions to the buttons

--- frontend/src/Pages/Game/GamePage.jsx
+++ frontend/src/Pages/Game/GamePage.jsx
@@ -5,14 +5,34 @@ import ProgressBar from '../../Components/Game/ProgressBar'
 
 export default function GamePage() {
 
+    const film = { title: 'Pulp Fiction', img: 'https://cdn.europosters.eu/image/750/posters/pulp-fiction-cover-i1288.jpg' }
+    const nextFilm = { title: 'Fight club', img: 'https://img.fruugo.com/product/0/08/14290080_max.jpg' }
     
+    const [ratedFilms, setRatedFilms] = useState([]);
 
+    function handleGraging(e) {
+        console.log('e.target.id: ' + e.target.id);
+    }
+
+    const propsForProgressBar = {
+        ratedFilms,
+    }
+
+    const propsForCarousel = {
+        film,
+        nextFilm,
+    }
+
+    const propsForGrades = {
+        handleGraging,
+    }
 
   return (
+    
     <div className='d-flex flex-column'>
-        <ProgressBar />
-        <Carousel />
-        <Grades />
+        <ProgressBar {...propsForProgressBar} />
+        <Carousel {...propsForCarousel} />
+        <Grades {...propsForGrades} />
     </div>
   )
 }
