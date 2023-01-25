6acb900520a2d42ec188f1567b324ca0c2307513
kgoodluck <k-goodluck@yandex.ru>
2023-01-25 17:46:25 +0200

    trying to get data from IMDB

--- frontend/src/Pages/Game/GamePage.jsx
+++ frontend/src/Pages/Game/GamePage.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import Carousel from '../../Components/Game/Carousel'
 import Grades from '../../Components/Game/Grades'
 import ProgressBar from '../../Components/Game/ProgressBar'
+import fetchMovieData from '../../utils/fetchMovieData'
 
 export default function GamePage() {
 
@@ -16,30 +17,50 @@ export default function GamePage() {
     const [currentFilm, setCurrentFilm] = useState({});
     const [nextFilm, setNextFilm] = useState({});
 
+    const [filmCounter, setFilmCounter] = useState(1);
+
     const [ratedFilms, setRatedFilms] = useState([]);
 
     function getMostPopularFilms() {
         // get 100 most rated films from DB
-        setMostPopularFilms([{movieId: 2, imdbId: '0113497', tmdbId: '8844'}, {movieId: 3, imdbId: '0113228', tmdbId: '15602'}, {movieId: 3, imdbId: '0114885', tmdbId: '31357'}, {movieId: 5, imdbId: '0113041', tmdbId: '11862'}, {movieId: 2, imdbId: '0113497', tmdbId: '8844'}, {movieId: 3, imdbId: '0113228', tmdbId: '15602'}, {movieId: 3, imdbId: '0114885', tmdbId: '31357'}, {movieId: 5, imdbId: '0113041', tmdbId: '11862'}, {movieId: 2, imdbId: '0113497', tmdbId: '8844'}, {movieId: 3, imdbId: '0113228', tmdbId: '15602'}, {movieId: 3, imdbId: '0114885', tmdbId: '31357'}, {movieId: 5, imdbId: '0113041', tmdbId: '11862'},])
+        setMostPopularFilms([{movieId: 2, imdbId: '0113497', tmdbId: '8844'}, {movieId: 3, imdbId: '0113228', tmdbId: '15602'}, {movieId: 4, imdbId: '0114885', tmdbId: '31357'}, {movieId: 5, imdbId: '0113041', tmdbId: '11862'}, {movieId: 2, imdbId: '0113497', tmdbId: '8844'}, {movieId: 3, imdbId: '0113228', tmdbId: '15602'}, {movieId: 3, imdbId: '0114885', tmdbId: '31357'}, {movieId: 5, imdbId: '0113041', tmdbId: '11862'}, {movieId: 2, imdbId: '0113497', tmdbId: '8844'}, {movieId: 3, imdbId: '0113228', tmdbId: '15602'}, {movieId: 3, imdbId: '0114885', tmdbId: '31357'}, {movieId: 5, imdbId: '0113041', tmdbId: '11862'}])
     }
 
     useEffect(() => {
         getMostPopularFilms();
-        setCurrentFilm(mostPopularFilms[0]);
-        setNextFilm(mostPopularFilms[1])
     }, [])
+
+    async function setInitialFilms() {
+        console.log('mostPopularFilms',mostPopularFilms)
+        console.log('mostPopularFilms[2]', mostPopularFilms[2]);
+        console.log('mostPopularFilms[2].imdbId', mostPopularFilms[2].imdbId);
+        const res = await fetchMovieData({imdbId: mostPopularFilms[2].imdbId});
+        console.log('res', res);
+    }
+
+    useEffect(() => {
+        setInitialFilms();
+        // setCurrentFilm(mostPopularFilms[filmCounter]);
+        // setNextFilm(mostPopularFilms[1]);
+    }, [mostPopularFilms])
+
 
     
 
     function handleGrading(e) {
-        if (e.target.id === 'skip-film') return fetchNewFilm();
+        if (e.target.id !== 'skip-film') setRatedFilms([...ratedFilms, { filmId: currentFilm.movieId, grade: e.target.id.split('-')[1] } ]);
         console.log('e.target.id: ' + e.target.id);
-        setRatedFilms([...ratedFilms, { filmId: film.id, grade: e.target.id.split('-')[1] } ])
+        setFilmCounter(prev => prev + 1);
+        setCurrentFilm(nextFilm);
+        setNextFilm(mostPopularFilms[filmCounter]);
     }
 
     useEffect(() => {
         console.log('ratedFilms', ratedFilms);
-    }, [ratedFilms])
+        console.log('currentFilm', currentFilm);
+        console.log('nextFilm', nextFilm);
+
+    }, [ratedFilms, currentFilm, nextFilm])
 
     function fetchNewFilm() {
         console.log('fetchNewFilm');
@@ -50,7 +71,7 @@ export default function GamePage() {
     }
 
     const propsForCarousel = {
-        film,
+        currentFilm,
         nextFilm,
     }
 
