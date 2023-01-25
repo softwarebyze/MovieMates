729254cd6346d5ed5f90901e9fbf0ea6e0c76188
Daveman20 <drdaveman1337@gmail.com>
2023-01-25 14:32:27 +0200

    implemented context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,56 @@
 import "./App.sass";
-import {BrowserRouter, Link, Routes, Route} from 'react-router-dom'
+import {useState, useContext} from 'react';
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Profile from "./Components/Profile";
 import GamePage from "./Pages/Game/GamePage";
 import PrivateRoute from "./Components/PrivateRoute";
+import MovieRec from "./Components/MovieRec";
+import AppContext from "./AppContext";
+import { AppProvider } from "./AppContext";
 
 function App() {
-  return (<BrowserRouter>
-    <div className="App">
-      <div className="NavBar">
-      <Link to='/'>Home</Link>
-      <Link to='/profile'>Profile</Link>
-      <Link to='game'>Rate Movies</Link>
+  const {handleLogout} = useContext(AppContext);
+ 
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <div className="NavBar">
+          <Link to="/">Home</Link>
+          <Link to="/game">Rate Movies</Link>
+          <Link to="/movieRec">Recommendations</Link>
+          <Link to="/profile">Profile</Link>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <Profile />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/game"
+            element={
+              <PrivateRoute>
+                <GamePage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/movieRec"
+            element={
+              <PrivateRoute>
+                <MovieRec />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
       </div>
-      <Routes><Route path="/" element={<Home/>} />
-      <Route path="/profile" element={<PrivateRoute><Profile/></PrivateRoute>}/>
-      <Route path="/game" element={<PrivateRoute><GamePage /></PrivateRoute>} /></Routes>
-      {/* <div className="NavBar">
-        <Link to="/">Home</Link>
-        <Link to="/game">Rate Movies</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/movieRec">Recommendations</Link>
-        <p>Logout</p>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        ></Route>
-        <Route
-          path="/game"
-          element={
-            <PrivateRoute>
-              <GamePage />
-            </PrivateRoute>
-          }
-        ></Route>
-        <Route
-          path="/profile"
-          element={
-            <PrivateRoute>
-              <Profile />
-            </PrivateRoute>
-          }
-        ></Route>
-        <Route
-          path="/movieRec"
-          element={
-            <PrivateRoute>
-              <MovieRec />
-            </PrivateRoute>
-          }
-        ></Route>
-      </Routes> */}
-    </div></BrowserRouter>
+    </BrowserRouter>
   );
 }
 
