9eb53dd5dfc84377f83134d7c625a16041fa19e1
Vlasta Vetra <vlastavetra@gmail.com>
2023-01-26 13:43:08 +0200

    fixed styles for user match page

--- frontend/src/Components/UserMatch/UserMatch.jsx
+++ frontend/src/Components/UserMatch/UserMatch.jsx
@@ -1,4 +1,5 @@
 import "./UserMatch.sass";
+import Footer from "../Footer";
 
 function UserMatch() {
   const users1 = JSON.parse(localStorage.getItem("userName"));
@@ -18,28 +19,34 @@ function UserMatch() {
       { name: "Movie 3", grade: "C" },
     ],
   };
+
   return (
-    <main className="main-usermatch-container">
-      <h1 className="usermatch-title">Movies match</h1>
-      <table className="usermatch-table">
-        <thead className="usermatch-table-head">
-          <tr className="usermatch-table-head-row">
-            <th className="usermatch-table-head-column">Movie</th>
-            <th className="usermatch-table-head-column">{users1}</th>
-            <th className="usermatch-table-head-column">{user2.name}</th>
-          </tr>
-        </thead>
-        <tbody className="usermatch-table-body">
-          {user1.movies.map((movie, index) => (
-            <tr key={index} className="usermatch-table-body-row">
-              <td className="usermatch-table-body-column">{movie.name}</td>
-              <td className="usermatch-table-body-column">{movie.grade}</td>
-              <td className="usermatch-table-body-column">{user2.movies[index].grade}</td>
+    <>
+      <main className="main-usermatch-container">
+        <h1 className="usermatch-title">Movies match</h1>
+        <table className="usermatch-table">
+          <thead className="usermatch-table-head">
+            <tr className="usermatch-table-head-row">
+              <th className="usermatch-table-head-column">Movie</th>
+              <th className="usermatch-table-head-column">{users1}</th>
+              <th className="usermatch-table-head-column">{user2.name}</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </main>
+          </thead>
+          <tbody className="usermatch-table-body">
+            {user1.movies.map((movie, index) => (
+              <tr key={index} className="usermatch-table-body-row">
+                <td className="usermatch-table-body-column">{movie.name}</td>
+                <td className="usermatch-table-body-column">{movie.grade}</td>
+                <td className="usermatch-table-body-column">
+                  {user2.movies[index].grade}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+      <Footer />
+    </>
   );
 }
 
